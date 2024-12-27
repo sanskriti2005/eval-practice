1ba13179a16b390dc23aabfd9161c360ef9b5ba8
Sanskriti <sanskritisri27@gmail.com>
2024-12-27 21:20:11 +0530

    built portable navbar

---
+++ backend/footer.js
@@ -0,0 +1,7 @@
+function loadFooter() {
+    const footer = document.getElementById("footer");
+    footer.innerHTML = `<footer>
+            <p>All Rights Reserved &copy; 2024</p>
+        </footer>`
+}
+loadFooter()
